a69ff73443840c7ed7d9bbb97840af81403b3560
Davis Allen <davis.b.allen@gmail.com>
2018-01-27 15:19:26 -0500

    Implement multi-planet gravity switching

---
+++ Assets/Scripts/NearestPlanet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearestPlanet : MonoBehaviour {
+	GameObject[] planets;
+	public GameObject closestPlanet;
+
+	// Use this for initialization
+	void Start () {
+		planets = GameObject.FindGameObjectsWithTag("Planet");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float minDist = 100f;
+		GameObject minDistObj = gameObject;
+
+		//find closest planet
+		foreach (GameObject obj in planets) {
+			float dist = Vector3.Distance(gameObject.transform.position, obj.transform.position);
+
+			//update current closest object seen so far
+			if (dist <= minDist) {
+				minDist = dist;
+				minDistObj = obj;
+			}
+		}
+
+		closestPlanet = minDistObj;
+	}
+}
