a44f45a4044fa90384570fe7aec6a6a5a7bf9ca5
Unknown <okh216@nyu.edu>
2018-01-27 17:40:35 -0500

    Added Detail animation and got everything synced up

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -12,7 +12,7 @@ public class AnimationController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
         currentState = anim.GetInteger("State");
 
