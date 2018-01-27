8ce88c29bafc3d16264dfc5600b39467f1f725d8
apleshak <pleshakov.anton@gmail.com>
2018-01-27 17:48:16 -0500

    Basic seed eating
    Need seed queue to finish up

---
+++ Assets/Scripts/EatSeed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatSeed : MonoBehaviour {
+	GameObject[] seeds;
+
+	// Use this for initialization
+	void Start () {
+		seeds = GameObject.FindGameObjectsWithTag("Seed");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		seeds = GameObject.FindGameObjectsWithTag("Seed");
+		float minDist = 100f;
+		GameObject minDistObj;
+
+		//find closest planet
+		foreach (GameObject obj in seeds) {
+			float dist = Vector3.Distance(gameObject.transform.position, obj.transform.position);
+
+			//update current closest object seen so far
+			if (dist <= minDist) {
+				minDist = dist;
+				minDistObj = obj;
+			}
+		}
+
+		if (minDistObj != null && Input.GetKeyDown("s")) {
+			GameObject.Destroy(minDistObj);
+		}
+	}
+}
