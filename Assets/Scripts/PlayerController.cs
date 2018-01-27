411a27104984b2a91a8cc5cedccc5887cb9db877
Unknown <okh216@nyu.edu>
2018-01-27 11:16:16 -0500

    Playing with movement
    Adding things to try and migate sliding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
     public float playerSpeed = 1.0f;
     public float jumpHeight = 1.0f;
     public bool onGround;
+    public bool peck = false;
+    public bool jumping;
     Rigidbody2D rb;
 	
     // Use this for initialization
@@ -18,20 +20,29 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.A)) //move left
+      if (Input.GetKeyDown(KeyCode.A)) //move left
         {
             rb.velocity = new Vector2 (-playerSpeed, 0);
         }
-    if (Input.GetKeyDown(KeyCode.D)) //move right
+      if (Input.GetKeyDown(KeyCode.D)) //move right
         {
             rb.velocity = new Vector2 (playerSpeed, 0);
         }
-    else if ((Input.GetKeyUp(KeyCode.A)) || (Input.GetKeyUp(KeyCode.D))) //If left or right are not pressed down, stop player momentum
+      //else if ((!Input.GetKeyDown(KeyCode.A)) || (!Input.GetKeyDown(KeyCode.A)))
+        //{
+          //  rb.velocity = new Vector2(0, 0);
+        //}
+      if (Input.GetKeyDown(KeyCode.S)) //peck
         {
-            rb.velocity = new Vector2 (0, 0);
+            peck = true;
         }
-    if (Input.GetKeyDown(KeyCode.Space)) //jump upward, no arc though?
+      else if (!Input.GetKeyDown(KeyCode.S)) //if not holding button, not pecking anymore
         {
+            peck = false;
+        }
+      if (Input.GetKeyDown(KeyCode.Space)) //jump upward, no arc though?
+        {
+            jumping = true;
             rb.velocity = new Vector2(0, jumpHeight);
         }
 	}
