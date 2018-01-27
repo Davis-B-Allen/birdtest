0706036512aa5035b3cc498b48b74c5b3fe19664
apleshak <pleshakov.anton@gmail.com>
2018-01-27 16:18:46 -0500

    Seed
    Added seed that grows into trees

---
+++ Assets/Scripts/Seed.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : MonoBehaviour {
+	public Object treePrefab;
+	public float treeGrowDelay = 2f;
+	bool canGrow = false;
+	ObjectGravity objGrav;
+
+	// Use this for initialization
+	void Start () {
+		objGrav = GetComponent<ObjectGravity>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		if (collision.gameObject.tag == "Planet") {
+			canGrow = true;
+		}
+	}
+
+	IEnumerator WaitForPlanet () {
+		while (!canGrow) {
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	public IEnumerator GrowTree () {
+		//wait until we hit the ground once (hopefully we'll stay there)
+		yield return WaitForPlanet();
+		//wait for some time before spawning tree and setting its planet, and parent
+		yield return new WaitForSeconds(treeGrowDelay);
+		GameObject tree = (GameObject) GameObject.Instantiate(treePrefab, transform.position, Quaternion.identity);//, (objGrav.closestPlanet.transform.position - transform.position).normalize);
+		GameObject.Destroy(gameObject);
+		TreeCreator tc = tree.GetComponent<TreeCreator>();
+		tc.planet = objGrav.closestPlanet;
+		tree.transform.parent = tc.planet.transform;
+	}
+}
