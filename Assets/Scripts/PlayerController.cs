c007bd339ccee4f84c5306b162c10e5376011f12
Unknown <okh216@nyu.edu>
2018-01-26 22:42:29 -0500

    Added PlayerController Script
    Started writing playercontroller script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float playerSpeed = 1.0f;
+    public float jumpHeight = 1.0f;
+    Rigidbody2D rb;
+	
+    // Use this for initialization
+	void Start ()
+    {
+    rb = GetComponent<Rigidbody2D>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+	if (Input.GetKeyDown(KeyCode.A)) //move left
+        {
+            rb.velocity = new Vector2(-playerSpeed, 0);
+        }
+    if (Input.GetKeyDown(KeyCode.D)) //move right
+        {
+            rb.velocity = new Vector2(playerSpeed, 0);
+        }
 	}
 }
