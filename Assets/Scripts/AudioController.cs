09d5c39ab4c40e06a43cdfbbbd5e029d31d8931f
Unknown <okh216@nyu.edu>
2018-01-28 11:18:42 -0500

    Added Sounds to Player
    Made audio controller script for player, added sounds

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour {
     AudioSource aud;
+    public AudioClip jumpSound;
+    public AudioClip peckSound;
+    public AudioClip wowSound;
 	// Use this for initialization
 	void Start () {
-		aud = GetComponent<AudioSource>()
+        aud = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.W))
         {
-            aud.
+            aud.PlayOneShot(jumpSound);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            aud.PlayOneShot(peckSound);
         }
 	}
 }
