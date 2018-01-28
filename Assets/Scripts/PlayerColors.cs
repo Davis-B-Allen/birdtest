02b8f5519de87fba3844c859a84788529deebb17
Peter Hechler <peter.hechler@gmail.com>
2018-01-28 12:33:12 -0500

    add player color ingestion list and let seeds add colors to it

---
+++ Assets/Scripts/PlayerColors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColors : MonoBehaviour {
+    public List<Color> ingestedColors;
+	// Use this for initialization
+	void Start () {
+        ingestedColors = new List<Color>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Color pullRandomColor () {
+        return ingestedColors[Random.Range(0, ingestedColors.Count - 1)];
+    }
+}
