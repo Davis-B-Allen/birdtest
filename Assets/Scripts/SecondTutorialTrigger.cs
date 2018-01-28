149e4b2a51e235ea248c8b085cf84b978f747bcb
Peter Hechler <peter.hechler@gmail.com>
2018-01-28 16:44:52 -0500

    fix duplicate tutorial

--- Assets/Scripts/SecondTutorialTrigger.cs
+++ Assets/Scripts/SecondTutorialTrigger.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class SecondTutorialTrigger : MonoBehaviour {
     GameObject player;
     public GameObject EatTutorial;
+    bool completed;
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
+        completed = false;
     }
 
     // Update is called once per frame
     void Update () {
-        if (Vector3.Distance(player.transform.position, transform.position) < 5f) {
+        if (!completed && Vector3.Distance(player.transform.position, transform.position) < 5f) {
             EatTutorial.GetComponent<FadeInOut>().startWithTrigger();
+            completed = true;
         }
     }
 }
