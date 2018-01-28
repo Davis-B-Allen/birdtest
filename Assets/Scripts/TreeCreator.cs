72f89a33d711f570a8fde1495006e59ed83369ee
Peter Hechler <peter.hechler@gmail.com>
2018-01-28 10:20:23 -0500

    allow tree to spawn from the ground and produce seeds from prefab

--- Assets/Scripts/TreeCreator.cs
+++ Assets/Scripts/TreeCreator.cs
@@ -7,18 +7,24 @@ public class TreeCreator : MonoBehaviour {
     public GameObject treeTrunk;
     public GameObject treeSegment;
     public GameObject planet;
+    public GameObject seed;
     Vector3 quadrant;
     public string growthType;
     //public float segmentInterval; 
     public float segmentCount;
+    bool creating;
+    Vector3 destination;
+    Vector3 directionBuried;
+    float growthSpeed;
 
 	void Start () {
-
+        creating = true;
         quadrant = new Vector3(1, 1, 0);
         float yDisplacement = transform.position.y - planet.transform.position.y;
         float xDisplacement = transform.position.x - planet.transform.position.x;
         float slope = yDisplacement / xDisplacement;
         float zRotation = Mathf.Rad2Deg * (Mathf.Atan(slope) - Mathf.PI / 2);
+
         if (xDisplacement < 0) {
             zRotation += 180f;
             quadrant.x = -1;
@@ -27,14 +33,30 @@ public class TreeCreator : MonoBehaviour {
             quadrant.y = -1;
         }
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, zRotation));
+        destination = transform.position;
+        //transform.position = planet.transform.position - transform.position / 2;
+        float xDirection = Mathf.Sin(Mathf.Deg2Rad * zRotation);
+        float yDirection = Mathf.Cos(Mathf.Deg2Rad * zRotation);
+        float obscuringDistance = ((segmentCount + 1) * 3.0f);
+        directionBuried = new Vector3(xDirection, -yDirection);
+        transform.position += (directionBuried * obscuringDistance);
+        growthSpeed = obscuringDistance / 50f;
+        //transform.localScale = new Vector3(.1f, 1f);
+
         if (growthType == "straightUp") {
             straightUp();
         }
+
 	}
 	
 	void Update () {
-		
-	}
+        if (creating) {
+            transform.position -= directionBuried * growthSpeed;
+            if (transform.position == destination) {
+                finishCreation();
+            }
+        }
+    }
 
     void initialize (GameObject trunk, GameObject segment, GameObject planetObj, string type, float interval, float count) {
         treeTrunk = trunk;
@@ -43,6 +65,26 @@ public class TreeCreator : MonoBehaviour {
         growthType = type;
         //segmentInterval = interval;
         segmentCount = count;
+    }
+
+    void finishCreation () {
+        creating = false;
+        //transform.localScale = new Vector3(1, 1);
+        dropSeeds();
+    }
+
+    void dropSeeds () {
+        Vector3 right = transform.GetChild(1).right;
+        Vector3 left = -1 * right;
+        Vector3 seedSpawn = directionBuried * .2f;
+        GameObject seed1 = Instantiate(seed);
+        GameObject seed2 = Instantiate(seed);
+        seed1.GetComponent<SpriteRenderer>().sortingOrder = 4;
+        seed2.GetComponent<SpriteRenderer>().sortingOrder = 4;
+
+        seed1.transform.position = transform.position + right * .5f;
+        seed2.transform.position = transform.position + left * .5f;
+        
     }
 
     void straightUp () {
@@ -91,7 +133,7 @@ public class TreeCreator : MonoBehaviour {
             //Vector3 segmentPosition = new Vector3(0, yCoord);
             //nextSegment.transform.localPosition = segmentPosition;
             nextSegment.transform.rotation = transform.rotation;
-            nextSegment.transform.localPosition = new Vector3(0, interval * i + 1);
+            nextSegment.transform.localPosition = new Vector3(0, interval * i + 3);
         }
     }
 
