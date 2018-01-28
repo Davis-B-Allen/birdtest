29f8f406bfd8b0bdfb57c1bf9b2b594d55208da9
Unknown <okh216@nyu.edu>
2018-01-28 10:55:14 -0500

    Scripting Audiocontroller
    Audio controller starting to be scripted

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioController : MonoBehaviour {
-
+    AudioSource aud;
 	// Use this for initialization
 	void Start () {
-		
+		aud = GetComponent<AudioSource>()
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.W))
+        {
+            aud.
+        }
 	}
 }
